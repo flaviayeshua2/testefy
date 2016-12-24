525e689ec77b114dc844a21a3d9d2214e89c9878
flaviayeshua <flaviayeshua@192.168.0.4>
2016-12-24 01:10:01 -0200

    Testando commit

---
+++ projetoGitLearn/src/projetoGitLearn/SegundaClasse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projetoGitLearn;
+
+/**
+ * @author flaviayeshua
+ *
+ */
+public class SegundaClasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Jesus");
+
+	}
+
+}
